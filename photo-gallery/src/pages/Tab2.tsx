a8bb6743d2f5d51e6df628256fe17c390bf57db9
atouloupis <atouloupis@yahoo.fr>
2021-01-04 13:44:56 +0100

    Take photo & display

--- photo-gallery/src/pages/Tab2.tsx
+++ photo-gallery/src/pages/Tab2.tsx
@@ -5,8 +5,9 @@ import './Tab2.css';
 import { camera, trash, close } from 'ionicons/icons';
 import { IonFab, IonFabButton, IonIcon, IonGrid, IonRow,
          IonCol, IonImg, IonActionSheet } from '@ionic/react';
-
+import { usePhotoGallery } from '../hooks/usePhotoGallery';
 const Tab2: React.FC = () => {
+  const { photos , takePhoto } = usePhotoGallery();
   return (
     <IonPage>
       <IonHeader>
@@ -15,6 +16,15 @@ const Tab2: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
+        <IonGrid>
+          <IonRow>
+            {photos.map((photo, index) => (
+              <IonCol size="6" key={index}>
+                <IonImg src={photo.webviewPath} />
+              </IonCol>
+            ))}
+          </IonRow>
+        </IonGrid>
         <IonFab vertical="bottom" horizontal="center" slot="fixed">
           <IonFabButton onClick={() => takePhoto()}>
             <IonIcon icon={camera}></IonIcon>
